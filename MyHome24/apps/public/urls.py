82a03181ee0b0c73e9f2b39fe8a730f47980e48d
Alexander <51765464+alechries@users.noreply.github.com>
2021-03-04 23:30:15 +0200

    Public URL without Views with redirect to public_index

--- MyHome24/apps/public/urls.py
+++ MyHome24/apps/public/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view),
+    path('', views.index_view, name='public_index'),
+    path('about', views.index_view, name='public_about'),
+    path('services', views.index_view, name='public_services'),
+    path('contact', views.index_view, name='public_contact'),
+    path('about', views.index_view, name='public_about'),
+    path('error', views.index_view, name='public_error'),
+    # path('account/', views.index_view, ),
 ]
