6fe498573fc00530c12647846db7c59518b79546
Alexander <51765464+alechries@users.noreply.github.com>
2021-03-04 23:46:44 +0200

    Public. Urls+views

--- MyHome24/apps/public/urls.py
+++ MyHome24/apps/public/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='public_index'),
-    path('about', views.index_view, name='public_about'),
-    path('services', views.index_view, name='public_services'),
-    path('contact', views.index_view, name='public_contact'),
-    path('about', views.index_view, name='public_about'),
-    path('error', views.index_view, name='public_error'),
-    #  path('account/', views.index_view, ),
+    path('about', views.about_view, name='public_about'),
+    path('services', views.services_view, name='public_services'),
+    path('contact', views.contact_view, name='public_contact'),
+    path('about', views.about_view, name='public_about'),
+    path('error', views.error_view, name='public_error'),
 ]
--- MyHome24/apps/public/views.py
+++ MyHome24/apps/public/views.py
@@ -4,3 +4,19 @@ from _db import models
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+def about_view(request):
+    return render(request, 'about.html')
+
+
+def services_view(request):
+    return render(request, 'services.html')
+
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+
+def error_view(request):
+    return render(request, 'error.html')
