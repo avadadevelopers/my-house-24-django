2b3b8dbfd7ad4f152f7d2dedb83e3327c6d819be
dimadima5182 <39956090+dimadima5182@users.noreply.github.com>
2021-03-10 17:41:57 +0200

    add login admin and cabinet form

--- MyHome24/apps/cabinet/views.py
+++ MyHome24/apps/cabinet/views.py
@@ -3,68 +3,68 @@ from _db import models
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'cabinet/index.html')
 
 
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, 'cabinet/login.html')
 
 
 def logout_view(request):
-    return render(request, 'logout.html')
+    return render(request, 'cabinet/logout.html')
 
 
 def statistic_view(request):
-    return render(request, 'statistic.html')
+    return render(request, 'cabinet/statistic.html')
 
 
 def invoice_view(request):
-    return render(request, 'invoice/index.html')
+    return render(request, 'cabinet/invoice/index.html')
 
 
 def invoice_view_view(request):
-    return render(request, 'invoice/view.html')
+    return render(request, 'cabinet/invoice/view.html')
 
 
 def tariffs_view(request):
-    return render(request, 'tariffs/index.html')
+    return render(request, 'cabinet/tariffs/index.html')
 
 
 def tariffs_view_view(request):
-    return render(request, 'tariffs/view.html')
+    return render(request, 'cabinet/tariffs/view.html')
 
 
 def messages_view(request):
-    return render(request, 'messages/index.html')
+    return render(request, 'cabinet/messages/index.html')
 
 
 def messages_view_view(request):
-    return render(request, 'messages/view.html')
+    return render(request, 'cabinet/messages/view.html')
 
 
 def messages_create_view(request):
-    return render(request, 'messages/create.html')
+    return render(request, 'cabinet/messages/create.html')
 
 
 def messages_delete_view(request):
-    return render(request, 'messages/delete.html')
+    return render(request, 'cabinet/messages/delete.html')
 
 
 def master_request_view(request):
-    return render(request, 'master-request/index.html')
+    return render(request, 'cabinet/master-request/index.html')
 
 
 def master_request_create_view(request):
-    return render(request, 'master-request/create.html')
+    return render(request, 'cabinet/master-request/create.html')
 
 
 def master_request_delete_view(request):
-    return render(request, 'master-request/delete.html')
+    return render(request, 'cabinet/master-request/delete.html')
 
 
 def user_view(request):
-    return render(request, 'user/index.html')
+    return render(request, 'cabinet/user/index.html')
 
 
 def user_change_view(request):
-    return render(request, 'user/change.html')
+    return render(request, 'cabinet/user/change.html')
