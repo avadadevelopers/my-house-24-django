459b7d86782cbed56318deabbd58f238b4d8f9a0
Alex <sanowese@gmail.com>
2021-04-08 16:05:16 +0300

    Docker

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,3 @@ RUN pip install -r requirements.txt
 
 # copy project
 COPY . .
-
-# run
-#EXPOSE 8000
-#CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- config/settings.py
+++ config/settings.py
@@ -66,18 +66,12 @@ AUTHENTICATION_BACKENDS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-        #############################################################################
-        # Make sure postgres is installed on your local machine before uncommenting #
-        #############################################################################
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'MyHouse24',
-        # 'USER': 'MyHouse24',
-        # 'PASSWORD': 'MyHouse24',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '5432',
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
